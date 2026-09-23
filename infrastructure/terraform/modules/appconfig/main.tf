a9bae065bdb254aa7879ad1ed29cd4da4fb8265b
Sakshee <98686393+Sakshee30@users.noreply.github.com>
2026-09-23 12:23:10 +0530

    Section 16: complete configuration and secret-service controls

--- infrastructure/terraform/modules/appconfig/main.tf
+++ infrastructure/terraform/modules/appconfig/main.tf
@@ -1,5 +1,32 @@
-resource "aws_appconfig_application" "this" { name = var.application_name tags = var.tags }
-resource "aws_appconfig_environment" "this" { application_id = aws_appconfig_application.this.id name = var.environment_name tags = var.tags }
-resource "aws_appconfig_configuration_profile" "this" { application_id = aws_appconfig_application.this.id name = var.configuration_profile_name location_uri = "hosted" type = "AWS.Freeform" }
-resource "aws_appconfig_hosted_configuration_version" "baseline" { application_id = aws_appconfig_application.this.id configuration_profile_id = aws_appconfig_configuration_profile.this.configuration_profile_id content_type = "application/json" content = jsonencode({ version = 1, capabilities = {} }) }
-resource "aws_appconfig_deployment" "baseline" { application_id = aws_appconfig_application.this.id environment_id = aws_appconfig_environment.this.environment_id configuration_profile_id = aws_appconfig_configuration_profile.this.configuration_profile_id configuration_version = aws_appconfig_hosted_configuration_version.baseline.version_number deployment_strategy_id = var.deployment_strategy_id }
+resource "aws_appconfig_application" "this" {
+  name = var.application_name
+  tags = var.tags
+}
+
+resource "aws_appconfig_environment" "this" {
+  application_id = aws_appconfig_application.this.id
+  name           = var.environment_name
+  tags           = var.tags
+}
+
+resource "aws_appconfig_configuration_profile" "this" {
+  application_id = aws_appconfig_application.this.id
+  name           = var.configuration_profile_name
+  location_uri   = "hosted"
+  type           = "AWS.Freeform"
+}
+
+resource "aws_appconfig_hosted_configuration_version" "baseline" {
+  application_id           = aws_appconfig_application.this.id
+  configuration_profile_id = aws_appconfig_configuration_profile.this.configuration_profile_id
+  content_type             = "application/json"
+  content                  = jsonencode(var.initial_configuration)
+}
+
+resource "aws_appconfig_deployment" "baseline" {
+  application_id           = aws_appconfig_application.this.id
+  environment_id           = aws_appconfig_environment.this.environment_id
+  configuration_profile_id = aws_appconfig_configuration_profile.this.configuration_profile_id
+  configuration_version    = aws_appconfig_hosted_configuration_version.baseline.version_number
+  deployment_strategy_id   = var.deployment_strategy_id
+}
