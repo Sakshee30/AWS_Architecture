108223c6f472d0fa36fded8f300fcd54f556649d
Sakshee <98686393+Sakshee30@users.noreply.github.com>
2026-09-23 12:23:05 +0530

    Section 16: complete configuration and secret-service controls

--- infrastructure/terraform/modules/secrets/outputs.tf
+++ infrastructure/terraform/modules/secrets/outputs.tf
@@ -1,1 +1,10 @@
-output "secret_metadata" { value = { for name, secret in aws_secretsmanager_secret.this : name => { arn = secret.arn, name = secret.name } } }
+output "secret_metadata" {
+  value = {
+    for name, secret in aws_secretsmanager_secret.this :
+    name => {
+      arn                = secret.arn
+      name               = secret.name
+      rotation_configured = contains(keys(var.rotation), name)
+    }
+  }
+}
