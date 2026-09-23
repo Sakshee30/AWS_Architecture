655c971767c3bed4743196431170891947632432
Sakshee <98686393+Sakshee30@users.noreply.github.com>
2026-09-23 12:32:50 +0530

    Section 17: complete container orchestration implementation aligned to sections 1-15

---
+++ infrastructure/terraform/modules/eks/variables.tf
@@ -0,0 +1,9 @@
+variable "cluster_name" { type = string }
+variable "cluster_version" { type = string default = "1.31" }
+variable "subnet_ids" { type = list(string) }
+variable "cluster_role_arn" { type = string }
+variable "node_role_arn" { type = string }
+variable "enable_gpu_nodes" { type = bool default = false }
+variable "standard_instance_types" { type = list(string) default = ["m6i.large"] }
+variable "gpu_instance_types" { type = list(string) default = ["g5.xlarge"] }
+variable "tags" { type = map(string) default = {} }
