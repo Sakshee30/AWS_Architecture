2db51daa9ab01d2813152ec318d7e972c0203b55
Sakshee <98686393+Sakshee30@users.noreply.github.com>
2026-09-23 12:18:55 +0530

    Section 16: implement AWS configuration and secret modules

---
+++ infrastructure/terraform/modules/kms/main.tf
@@ -0,0 +1,7 @@
+resource "aws_kms_key" "this" {
+ description = var.description
+ enable_key_rotation = true
+ deletion_window_in_days = var.deletion_window_in_days
+ tags = var.tags
+}
+resource "aws_kms_alias" "this" { name = "alias/${var.alias_name}" target_key_id = aws_kms_key.this.key_id }
