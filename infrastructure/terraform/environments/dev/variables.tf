48894b752c5433dd57740c15d1b8dd26b831c8c9
Sakshee <98686393+Sakshee30@users.noreply.github.com>
2026-09-23 12:23:16 +0530

    Section 16: complete configuration and secret-service controls

--- infrastructure/terraform/environments/dev/variables.tf
+++ infrastructure/terraform/environments/dev/variables.tf
@@ -5,3 +5,21 @@ variable "cost_center" { type=string }
 variable "audit_bucket_name" { type=string }
 variable "non_secret_parameters" { type=map(string) default={} }
 variable "secret_names" { type=set(string) default=[] }
+variable "secret_rotation" {
+  type = map(object({
+    lambda_arn = string
+    days       = number
+  }))
+  default = {}
+}
+variable "initial_configuration" {
+  type = object({
+    version = number
+    capabilities = map(object({
+      enabled  = bool
+      provider = string
+      fallback = optional(string)
+    }))
+  })
+  default = { version = 1, capabilities = {} }
+}
