257444663843e038835f7e0ddd03289bf424cab0
Sakshee <98686393+Sakshee30@users.noreply.github.com>
2026-09-23 12:18:41 +0530

    Section 16: implement AWS configuration and secret modules

---
+++ infrastructure/terraform/modules/appconfig/outputs.tf
@@ -0,0 +1,3 @@
+output "application_id" { value = aws_appconfig_application.this.id }
+output "environment_id" { value = aws_appconfig_environment.this.environment_id }
+output "configuration_profile_id" { value = aws_appconfig_configuration_profile.this.configuration_profile_id }
