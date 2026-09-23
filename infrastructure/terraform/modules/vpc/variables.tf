0253b605cb6998b2d23e81c130e4032dea041a3d
Sakshee <98686393+Sakshee30@users.noreply.github.com>
2026-09-23 13:07:18 +0530

    Section 18: implement core Terraform modules

---
+++ infrastructure/terraform/modules/vpc/variables.tf
@@ -0,0 +1,8 @@
+variable "name" { type = string }
+variable "cidr" { type = string default = "10.0.0.0/16" }
+variable "azs" { type = list(string) }
+variable "public_subnet_cidrs" { type = list(string) }
+variable "private_app_subnet_cidrs" { type = list(string) }
+variable "isolated_data_subnet_cidrs" { type = list(string) }
+variable "enable_nat_gateway" { type = bool default = true }
+variable "tags" { type = map(string) default = {} }
