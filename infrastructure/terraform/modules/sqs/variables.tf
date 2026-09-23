9c0b9784976d2fc0fe7d20cadd2195538c81180e
Sakshee <98686393+Sakshee30@users.noreply.github.com>
2026-09-23 13:07:44 +0530

    Section 18: implement core Terraform modules

---
+++ infrastructure/terraform/modules/sqs/variables.tf
@@ -0,0 +1,6 @@
+variable "enabled" { type = bool default = true }
+variable "name" { type = string }
+variable "visibility_timeout_seconds" { type = number default = 60 }
+variable "max_receive_count" { type = number default = 5 }
+variable "kms_key_id" { type = string default = null }
+variable "tags" { type = map(string) default = {} }
