d1aa4b811c8af73343a63addc80543eef8b02b29
Sakshee <98686393+Sakshee30@users.noreply.github.com>
2026-09-23 12:18:52 +0530

    Section 16: implement AWS configuration and secret modules

---
+++ infrastructure/terraform/modules/kms/variables.tf
@@ -0,0 +1,4 @@
+variable "alias_name" { type = string }
+variable "description" { type = string default = "Platform managed encryption key" }
+variable "deletion_window_in_days" { type = number default = 30 }
+variable "tags" { type = map(string) default = {} }
