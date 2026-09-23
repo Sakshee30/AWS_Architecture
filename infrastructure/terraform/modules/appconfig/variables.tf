0d9d20883b7bf7c6216d19041d7fc5d2033db06f
Sakshee <98686393+Sakshee30@users.noreply.github.com>
2026-09-23 12:23:08 +0530

    Section 16: complete configuration and secret-service controls

--- infrastructure/terraform/modules/appconfig/variables.tf
+++ infrastructure/terraform/modules/appconfig/variables.tf
@@ -2,4 +2,15 @@ variable "application_name" { type = string }
 variable "environment_name" { type = string }
 variable "configuration_profile_name" { type = string default = "platform-capabilities" }
 variable "deployment_strategy_id" { type = string default = "AppConfig.Linear20PercentEvery6Minutes" }
+variable "initial_configuration" {
+  type = object({
+    version = number
+    capabilities = map(object({
+      enabled  = bool
+      provider = string
+      fallback = optional(string)
+    }))
+  })
+  default = { version = 1, capabilities = {} }
+}
 variable "tags" { type = map(string) default = {} }
